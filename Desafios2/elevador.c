ef5590ec105ec1d134dc3c6e6111af60baece5cd
Leandro Araújo <leandro.arauj047.edu@gmail.com>
2021-04-21 15:27:07 -0300

    Atualizando

--- Desafios2/elevador.c
+++ Desafios2/elevador.c
@@ -4,17 +4,15 @@ int main() {
     int leitura_sensor, capacidade;
     int saida[1000], entrada[1000], limite=0, excesso=0;
 
-    saida[-1] = 0;
-
     scanf("%d%d", &leitura_sensor, &capacidade);
 
     for (int i = 0; i < leitura_sensor; ++i) {
         scanf("%d%d", &saida[i], &entrada[i]);
-        limite = entrada[i] - saida[i-1]; 
+        limite = limite + entrada[i] - saida[i]; 
         if (limite > capacidade) {
             excesso += 1;
         }
-        printf("%d\n", limite);
+        printf("%d\n\n", limite);
     }
 
     if (excesso == 0) {
