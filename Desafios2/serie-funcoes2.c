770b5f32c4a1b19eacc056dae57f942ebdd9eb53
Leandro Araújo <leandro.arauj047.edu@gmail.com>
2021-05-05 23:31:06 -0300

    Adicionando

--- Desafios2/serie-funcoes2.c
+++ Desafios2/serie-funcoes2.c
@@ -19,6 +19,14 @@ int eh_primo(int x) {
     return eh_primo_i(2, x);
 }
 
+int proximo_primo(int y) {
+    if (eh_primo(y + 1) == 1) {
+        return y + 1;
+    }
+
+    return proximo_primo(y + 1);
+}
+
 int fatorial(int z) {
     if(z == 0) {
         return 1;
@@ -36,6 +44,7 @@ int main() {
 
     printf("fat(%d) = %d\n", n, fatorial(n));
     printf("eh_primo(%d) = %d\n", n, eh_primo(n));
+    printf("Proximo_primo(%d) = %d\n", n, proximo_primo(n));
 
     return 0;
 }
