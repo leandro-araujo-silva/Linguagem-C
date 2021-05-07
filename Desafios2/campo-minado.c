ff68a36b7ba645f610a3ad84ee3acac2d9118d80
Leandro Araújo <leandro.arauj047.edu@gmail.com>
2021-05-06 22:08:39 -0300

    Adicionando

---
+++ Desafios2/campo-minado.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+void inicializarJogo(int linha[], int coluna[], int tam) {
+    
+}
+
+int main() {
+    int linha[10], coluna[10], tam;
+
+    scanf("%d", &tam);
+
+    inicializarJogo(linha, coluna, tam);
+
+    return;
+}
