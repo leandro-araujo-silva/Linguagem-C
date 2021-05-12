7ec0193b32adc035bca7cc0c721c7fd499366421
Leandro Araújo <leandro.arauj047.edu@gmail.com>
2021-05-12 16:23:59 -0300

    Adicionando

---
+++ Desafios2/contar-palavras.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+void escanear(char array[], int resultado) {
+    if(scanf("%s", array) != EOF) {
+        resultado++;
+        escanear(array, resultado);
+    } else {
+        printf("%d", resultado);
+    }
+}
+
+int main() {
+    char array[500];
+    escanear(array, 0);
+    return 0;
+}
