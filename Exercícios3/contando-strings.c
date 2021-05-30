b55bba4671b91b1441e91e29920a3217657cff39
Leandro Araújo <leandro.arauj047.edu@gmail.com>
2021-05-30 18:14:47 -0300

    Adicionando

--- Exercícios3/contando-strings.c
+++ Exercícios3/contando-strings.c
@@ -6,19 +6,19 @@ int main() {
     char texto[50];
     int c, conta = 0;
 
-    scanf("%s", texto);
+    fgets(texto, 150, stdin);
 
     c = strlen(texto);
 
-    printf("Numero de letras = %d", c);
+    printf("Numero de letras = %d\n", c);
 
     for(int i = 0; texto[i] != '\0'; i++) {
-        if((texto ==' ') || (texto == '\n')) {
-            conta++;
+        if (texto ==' '){
+            ++conta;
         }
     }
 
-    printf("Conta = %d", conta);
+    printf("Conta = %d\n", conta+1);
 
     return 0;
 }
